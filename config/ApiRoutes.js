031a80aae336d330d25fa69350bf9b861baa73c0
Rishabh Anand <ranand16@gmail.com>
2021-11-29 17:56:31 +0530

    bugs fixed

--- config/ApiRoutes.js
+++ config/ApiRoutes.js
@@ -1,6 +1,6 @@
 import { BASE_SERVER_V1_API } from "./Constants";
 
-const generateDetailsApiRoute = () => `${BASE_SERVER_V1_API}/`;
+const generateDetailsApiRoute = () => `${BASE_SERVER_V1_API}/review`;
 export {
     generateDetailsApiRoute
 }
