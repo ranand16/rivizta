41f0cd23e3ad480b0b0e9f33ef56ed21f1a52061
Rishabh Anand <ranand16@gmail.com>
2021-12-02 01:59:25 +0530

    env files update

--- config/Constants.js
+++ config/Constants.js
@@ -1,2 +1,2 @@
-export const SERVER_BASE_URL = process.env.SERVER_BASE_URL || 'http://13.250.30.36';
+export const SERVER_BASE_URL = process.env.SERVER_BASE_URL || 'http://13.213.55.228';
 export const BASE_SERVER_V1_API = `${SERVER_BASE_URL}/v1`;
