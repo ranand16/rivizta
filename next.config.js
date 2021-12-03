54736c3f7515772d08fd8f91bbe6f874179e6d5c
Rishabh Anand <ranand16@gmail.com>
2021-12-03 07:40:06 +0530

    added env variable

--- next.config.js
+++ next.config.js
@@ -5,5 +5,6 @@ module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
   },
+  env: {  SERVER_BASE_URL: process.env.SERVER_BASE_URL  },
   reactStrictMode: true,
 }
