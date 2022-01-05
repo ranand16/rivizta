a0b22854df668a6fa85dd634ad43a68b575b5afe
Rishabh Anand <ranand16@gmail.com>
2022-01-05 09:52:48 +0530

    Added page to accept payment request

--- config/ApiRoutes.js
+++ config/ApiRoutes.js
@@ -1,7 +1,11 @@
+import { BASE_SERVER_V1_API } from "./Constants";
+
 const generateLotteryParticipateApiRoute = () => `/lottery`;
 const generateAddProductApiRoute = () => `/product`;
+const generatePaymentDemandApiRoute = () => `${BASE_SERVER_V1_API}/payment-demand`;
 
 export {
     generateLotteryParticipateApiRoute,
-    generateAddProductApiRoute
+    generateAddProductApiRoute,
+    generatePaymentDemandApiRoute
 }
---
+++ pages/a8d46d341bea4fd5bff866a65ff8bfb0.tsx
@@ -0,0 +1,46 @@
+import Axios from "axios";
+import { GetServerSideProps, NextPage } from "next";
+import React from "react";
+import { generatePaymentDemandApiRoute } from "../config/ApiRoutes";
+
+interface Props {
+    data: string | null,
+    error: any
+}
+
+const PaymentRequest: NextPage<Props> = ({ data, error } : Props) => {
+    console.log(data, error);
+    return <>{error || data || "Processing" }</>;
+}
+
+export default PaymentRequest;
+
+export const getServerSideProps: GetServerSideProps = async ({ query }) => {
+    const refinedQuery = new URLSearchParams(query as Record<any, any>); // since Next js assigns query as Dict<any, any>
+    // upi://pay?pa=9886142140@upi&pn=Myself&mc=payeeMCC&tid=trxnID&tr=trxnRefId&tn=This is a note&am=1&cu=INR&refUrl=refUrl&mode=04&purpose=00
+    // const config = { headers: { authorization: `${token}` } };
+
+    if(!(refinedQuery.get("pa") && refinedQuery.get("pn") && refinedQuery.get("am") && refinedQuery.get("tn"))) {
+        return {
+            props: { 
+                error: "pa, pn, am and tn are compulsory fields for a payment demand request",
+                data: null,
+            }
+        }
+    }
+    const refinedQueryEntries = refinedQuery.entries();
+    console.log(refinedQueryEntries)
+    const refinedQueryObject: any = {};
+    for(const [key, value] of refinedQueryEntries) { // each 'entry' is a [key, value] tupple
+        refinedQueryObject[key] = value;
+    }
+    const paymentURL = `upi://pay?pa=${refinedQuery.get("pa")}&pn=${refinedQuery.get("pn")}&mc=${refinedQuery.get("mc")}&tid=${refinedQuery.get("tid")}&tr=${refinedQuery.get("tr")}&tn=${refinedQuery.get("tn")}&cu=${refinedQuery.get("cu")}&refUrl=${refinedQuery.get("refUrl")}&mode=${refinedQuery.get("mode")}&purpose=${refinedQuery.get("purpose")}`;
+    const { data } = await Axios.post(generatePaymentDemandApiRoute(),{ paymentURL, refinedQuery: refinedQueryObject });
+    console.log(data);
+    return {
+        props: {
+            error: null,
+            data: "Success"
+        }
+    }
+}
