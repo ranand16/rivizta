cdb6cd9d2446c849ac8b0b825c9019739533b443
Rishabh Anand <ranand16@gmail.com>
2022-01-09 10:56:58 +0530

    added not  confimred params

--- pages/a8d46d341bea4fd5bff866a65ff8bfb0.tsx
+++ pages/a8d46d341bea4fd5bff866a65ff8bfb0.tsx
@@ -56,7 +56,7 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
         for(const [key, value] of refinedQueryEntries) { // each 'entry' is a [key, value] tupple
             refinedQueryObject[key] = value;
         }
-        const paymentURL = `upi://pay?pa=${refinedQuery.get("pa")}&pn=${refinedQuery.get("pn")}&mc=${refinedQuery.get("mc")}&tid=${refinedQuery.get("tid")}&tr=${refinedQuery.get("tr")}&tn=${refinedQuery.get("tn")}&am=${refinedQuery.get("am")}&cu=${refinedQuery.get("cu")}&refUrl=${refinedQuery.get("refUrl")}&mode=${refinedQuery.get("mode")}&purpose=${refinedQuery.get("purpose")}`;
+        const paymentURL = `upi://pay?pa=${refinedQuery.get("pa")}&pn=${refinedQuery.get("pn")}&tn=${refinedQuery.get("tn")}&am=${refinedQuery.get("am")}&cu=${refinedQuery.get("cu")}&mode=${refinedQuery.get("mode")}&purpose=${refinedQuery.get("purpose")}`;
         const { data } = await Axios.post(generatePaymentDemandApiRoute(),{ payment_url: paymentURL,  ...refinedQueryObject });
         return {
             props: {
