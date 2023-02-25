fdb7ef7f5225373cce5e600f97d022dab18e6dd2
Rishabh Anand <ranand16@gmail.com>
2023-02-25 19:56:18 +0530

    update cors for superpower

--- pages/superpower.tsx
+++ pages/superpower.tsx
@@ -132,10 +132,13 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
 
   console.info(`superpower.tsx -- ${req.headers['user-agent']}  -- ${new Date()}`);
   const superpowerRoute = generateGetAllSuperpowerApiRoute();
-  res.setHeader("Access-Control-Allow-Credentials","true");
-  res.setHeader("Access-Control-Allow-Origin","*");
-  res.setHeader("Access-Control-Allow-Methods","GET,OPTIONS,PATCH,DELETE,POST,PUT");
-  res.setHeader("Access-Control-Allow-Headers","X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  )
   try {
       // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
       const resp = await axios.get(superpowerRoute, {
