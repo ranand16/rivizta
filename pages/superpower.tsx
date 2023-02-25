1d6f8840d41da2990f344735731d98a4da3d350a
Rishabh Anand <ranand16@gmail.com>
2023-02-25 20:00:10 +0530

    ipdate api cors policy

--- pages/superpower.tsx
+++ pages/superpower.tsx
@@ -143,8 +143,12 @@ export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
       // axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
       const resp = await axios.get(superpowerRoute, {
         headers: {
+          'Access-Control-Allow-Credentials': 'true',
           'Access-Control-Allow-Origin': '*',
           'Content-Type': 'application/json',
+          'Access-Control-Allow-Methods':'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+          'Access-Control-Allow-Headers':'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  
         }
       });
       console.log(resp.data.data);
