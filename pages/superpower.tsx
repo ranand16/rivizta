cfdec666a7c931c97e7a8f44ffbd530c5f06b0f5
Rishabh Anand <ranand16@gmail.com>
2023-02-25 16:57:32 +0530

    updated res header for superpowere

--- pages/superpower.tsx
+++ pages/superpower.tsx
@@ -128,10 +128,14 @@ const Home: NextPage = ({superpowerData: spd}: any) => {
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+export const getServerSideProps: GetServerSideProps = async ({ req, res }) => {
+
   console.info(`superpower.tsx -- ${req.headers['user-agent']}  -- ${new Date()}`);
   const superpowerRoute = generateGetAllSuperpowerApiRoute();
-  console.log("This is the route :: ", superpowerRoute);
+  res.setHeader("Access-Control-Allow-Credentials","true");
+  res.setHeader("Access-Control-Allow-Origin","*");
+  res.setHeader("Access-Control-Allow-Methods","GET,OPTIONS,PATCH,DELETE,POST,PUT");
+  res.setHeader("Access-Control-Allow-Headers","X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version");
   try {
       const resp = await axios.get(superpowerRoute, {});
       console.log(resp.data.data);
