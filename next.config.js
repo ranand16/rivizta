65e7df8c4729d67e0c87f05fe9f161ecf12831be
Rishabh Anand <ranand16@gmail.com>
2023-02-25 16:50:50 +0530

    *

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: "/*",
+        source: "*",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           { key: "Access-Control-Allow-Origin", value: "*" },
